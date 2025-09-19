2c72d7a2fa0f2618f2ece77d992eafed75a6f093
woojunhyeok <wjh7904@gmail.com>
2025-09-19 04:48:36 +0000

    Add package.json for Node.js app

--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const server = http.createServer((req,res)=>{res.end('Hello from ECS Fargate via GitHub CI/CD!\n');});
 server.listen(port);
